a0501015ab9b16e887908e78990bfcfdca079120
Chris Cornutt <enygma@phpdeveloper.org>
2011-02-14 22:25:06 +0000

    update towards MySQL connectivity

--- lib/database/Mysql.class.php
+++ lib/database/Mysql.class.php
@@ -8,6 +8,19 @@ class Database_Mysql extends Database
 		//nothing to see here
 	}
 
+	public function connect()
+	{
+		$arr = array(
+			'database.username',
+			'database.password',
+			'database.name',
+			'database.host'
+		);
+		$values = Configure::getConfigValue($arr);
+		//print_r($values);		
+
+		//TODO make the mysql connection here
+	}
 }
 
 ?>
