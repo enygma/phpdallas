8f2396b1f8e781bcf623693c185758dd7aae1615
Chris Cornutt <enygma@phpdeveloper.org>
2011-02-16 22:17:25 +0000

    adding start of user functionality

---
+++ site/controller/User.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class Controller_User extends Controller
+{
+
+	public function __construct()
+	{
+		//nothing to see here...
+	}
+
+	public function index()
+	{
+	}
+
+	public function login()
+	{
+	}
+
+	public function signup()
+	{
+		// signup options - basic, twitter, facebook
+	}
+
+}
+
+?>
---
+++ site/view/user/signup.php
@@ -0,0 +1,8 @@
+<h2>Signup</h2>
+
+<?php
+
+echo $this->html->form->text('username');
+echo $this->html->form->text('password');
+
+?>
