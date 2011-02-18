9c245c35680c51b313f0166b46856059e5460910
Chris Cornutt <enygma@phpdeveloper.org>
2011-02-18 22:26:58 +0000

    updating initial SQL load

--- assets/sql/initial_load.sql
+++ assets/sql/initial_load.sql
@@ -1,2 +1,17 @@
 # Initial database structure
-create table meetings (title varchar(100), detail TEXT, meeting_date INT, ID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(ID));
+create table `meetings` (
+	title varchar(100), 
+	detail TEXT, 
+	meeting_date INT, 
+	ID INT NOT NULL AUTO_INCREMENT, 
+	PRIMARY KEY(ID)
+);
+
+create table `users` (
+	username VARCHAR(50),
+	password VARCHAR(40),
+	email VARCHAR(100),
+	full_name VARCHAR(100),
+	ID INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY(ID)
+);
