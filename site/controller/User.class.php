25e72eb6447d77d2eb7b58ff9414940c44ef37e9
Chris Cornutt <enygma@phpdeveloper.org>
2011-02-18 22:27:19 +0000

    updates for login and creation of users

--- site/controller/User.class.php
+++ site/controller/User.class.php
@@ -12,8 +12,61 @@ class Controller_User extends Controller
 	{
 	}
 
+	public function create()
+	{
+
+		$this->useTemplate = false;
+
+		$user = new UserModel();
+                $user->username = 'test';
+                $user->password = 'here';
+                $user->full_name = 'full name';
+                $user->email    = 'me@me.com';
+                //$user->create();
+
+		$user = new UserModel(1);
+
+		return true;
+
+		$this->useTemplate = false;
+		$valid 	= new Validaton();
+		$posted = $this->filter->post();
+
+		$valid->setValidation(array(
+			'username' 	=> 'required',
+			'password' 	=> 'required',
+			'full_name'	=> 'required',
+			'email' 	=> 'required|email'
+		));
+
+		if($this->filter->post('sub') && $valid->validate($posted)){
+			$user = new UserModel();
+
+			$user->username = $this->filter->post('username');
+			$user->password = $this->filter->post('password');
+			$user->full_name = $this->filter->post('full_name');
+			$user->email	= $this->filter->post('email');
+
+			$user->create();
+
+			/*
+			$db = new Database();
+			$db->insert('users',array(
+				'username'	=> $this->filter->post('username'),
+				'password'	=> md5($this->filter->post('password')),
+				'full_name'	=> $this->filter->post('full_name'),
+				'email'		=> $this->filter->post('email')
+			);
+			*/
+		}
+	}
+
 	public function login()
 	{
+		$this->useTemplate = false;
+		$valid 	= new Validation();
+		$posted = $this->filter->post();
+
 	}
 
 	public function signup()
