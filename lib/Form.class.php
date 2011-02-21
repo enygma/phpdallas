ba942d251110e9fe0dbde4b4fe8adfe82533a21c
Chris Cornutt <enygma@phpdeveloper.org>
2011-02-20 18:40:46 -0600

    adding the form helper class

---
+++ lib/Form.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class Form
+{
+	public $definition = array();
+	
+	public function __construct()
+	{
+		// nothing to see here
+	}
+	
+	public function render()
+	{
+		var_dump($this);
+	}
+	
+}
+
+?>
