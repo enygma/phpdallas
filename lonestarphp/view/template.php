931251a91e5124cfb46ea420cc4cf5586ce32e0b
Eric Hyland <eric.hyland@gmail.com>
2011-03-24 11:02:04 -0500

    html5 logo
    Signed-off-by: Eric Hyland <eric.hyland@gmail.com>

--- lonestarphp/view/template.php
+++ lonestarphp/view/template.php
@@ -212,11 +212,10 @@
 				</tbody>
 			</table>
 			<br /><br /><br />
-				<a href="http://www.w3.org/html/logo/">
+			<a href="http://www.w3.org/html/logo/">
 				<img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" width="165" height="64" alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics">
-				</a>
+			</a>
 		</section>
-		
 	</div>
     
 	<footer>
