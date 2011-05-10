462c1ccadf703f37dd1b557cfdd70522224c6f24
Chris Cornutt <enygma@phpdeveloper.org>
2011-05-10 13:57:35 +0000

    adding giveaway controller and view

---
+++ lonestarphp/controller/Giveaway.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class Controller_Giveaway extends Controller
+{
+
+        public function __construct()
+        {
+        }
+
+	public function index()
+	{ 
+		$this->useTemplate = false;
+	}
+
+}
+?>
